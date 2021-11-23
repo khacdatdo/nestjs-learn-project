e9f0844e16e379beb168e10d89a0fbd15748931e
Đỗ Khắc Đạt <datdk.b19cn170@stu.ptit.edu.vn>
2021-11-23 20:30:28 +0700

    update

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(PORT);
+
+  console.log('Server running on port ' + PORT);
+}
+bootstrap();
