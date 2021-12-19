29a41e6d1a71aff378d1168b2b68724bc0ad26d1
Đỗ Khắc Đạt <datdk.b19cn170@stu.ptit.edu.vn>
2021-12-19 15:24:13 +0700

    Update greeting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, UsePipes } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AllValidationPipe } from './common/pipes/validation.pipe';
 
 @Controller()
 export class AppController {
@@ -9,11 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post()
-  @UsePipes(new AllValidationPipe())
-  getBadRequest(): string {
-    return this.appService.getBadRequest();
   }
 }
